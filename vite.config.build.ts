4099aa6020817e8c72ec033a471700b90563fac6
allan91coder <allan91coder@gmail.com>
2023-10-29 16:16:54 +0800

    feat: update

--- vite.config.build.ts
+++ vite.config.build.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 // import copyPlugin from 'rollup-plugin-copy'
 
 export default defineConfig({
-	// base: "./",
+	base: "./",
 	build: {
 		// emptyOutDir: false, // 复制文件夹，需要将此配置项设为 false
 		rollupOptions: {
