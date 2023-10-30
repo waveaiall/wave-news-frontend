55f03c652b2fac1f3db8db157ba8a60e1e10ddcb
xsf0105 <allan91coder@gmail.com>
2023-10-30 11:18:21 +0800

    feat: update

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,20 @@
-// import { QuarkElement, customElement } from "quarkc"
-// import style from "./index.less?inline"
+import { QuarkElement, customElement } from "quarkc"
+import style from "./index.less?inline"
 
-// import './app-header'
+import './app-main'
 // import './app-footer'
 // import './app-piano'
 
-// @customElement({ tag: "my-app", style })
-// class MyApp extends QuarkElement {
-//   render() {
-//     return (
-//       <div class="app">
-//       <app-header />
-//       <app-piano />
-//       <app-footer />
-//     </div>
-//     );
-//   }
-// }
+@customElement({ tag: "my-app", style })
+class MyApp extends QuarkElement {
+  render() {
+    return (
+      <div class="app">
+      <app-main />
+    </div>
+    );
+  }
+}
 
 // declare global {
 //   interface HTMLElementTagNameMap {
@@ -24,33 +22,32 @@
 //   }
 // }
 
-import { Router } from "@vaadin/router";
-import "./index.less";
+// import { Router } from "@vaadin/router";
+// import "./index.less";
 
-import "./app-login";
-import "./app-main";
-// import "./app-home";
+// import "./app-login";
+// import "./app-main";
 
-const outlet = document.querySelector("#root");
-export const router = new Router(outlet);
+// const outlet = document.querySelector("#root");
+// export const router = new Router(outlet);
 
-router.setRoutes([
-	{
-		path: "/",
-		component: "app-login",
-		action: async () => {
-			await import("./app-login");
-		},
-	},
-	{
-		path: "/main",
-		component: "app-main", // custom element name
-		action: async () => {
-			await import("./app-main");
-		},
-	},
-	// {
-	// 	path: "(.*)",
-	// 	component: "app-not-found",
-	// },
-]);
+// router.setRoutes([
+// 	{
+// 		path: "/",
+// 		component: "app-login",
+// 		action: async () => {
+// 			await import("./app-login");
+// 		},
+// 	},
+// 	{
+// 		path: "/main",
+// 		component: "app-main", // custom element name
+// 		action: async () => {
+// 			await import("./app-main");
+// 		},
+// 	},
+// 	// {
+// 	// 	path: "(.*)",
+// 	// 	component: "app-not-found",
+// 	// },
+// ]);
