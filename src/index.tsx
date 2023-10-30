878067f0b8160064eeca60ed1ad26e50fb7a46db
xsf0105 <allan91coder@gmail.com>
2023-10-30 16:00:37 +0800

    feat: update

--- src/index.tsx
+++ src/index.tsx
@@ -1,53 +1,82 @@
-import { QuarkElement, customElement } from "quarkc"
+import { QuarkElement, customElement, state } from "quarkc"
 import style from "./index.less?inline"
 
-import './app-main'
-// import './app-footer'
-// import './app-piano'
+import VConsole from 'vconsole';
+const vConsole = new VConsole({ theme: 'dark' });
+
+
+// import './app-main'
+
 
 @customElement({ tag: "my-app", style })
 class MyApp extends QuarkElement {
-  render() {
-    return (
-      <div class="app">
-      <app-main />
-    </div>
-    );
+
+  @state()
+  textContent: string = ''
+
+  @state()
+  loading: Boolean = false
+
+  componentDidMount() {
+    const btn =    this.shadowRoot.querySelector("#start-btn")
+    const result = this.shadowRoot.querySelector("#result")
+    const _this = this;
+
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+    var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+    var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+    const recognition = new SpeechRecognition()
+    recognition.continuous = false
+    recognition.lang = 'en-US'
+    recognition.interimResults = false
+    recognition.maxAlternatives = 1
+
+    recognition.onresult = (e) => {
+        const text = event.results[0][0].transcript
+        // onSuccess(text)
+        btn.className = "btn"
+        _this.textContent = text
+        _this.loading = false
+    }
+
+
+    recognition.onspeechend = function() {
+        // onError("未识别...")
+        recognition.stop()
+    }
+
+    recognition.onerror = function(event) {
+        // onError('识别错误: ' + event.error)
+
+        result.className = "result error"
+        _this.textContent = event.error
+        _this.loading = false
+    }
+
+
+    btn.addEventListener("click", (e) => {
+        e.target.className += " start"
+        recognition.start()
+        // btn.textContent = "识别中..."
+        this.loading = true
+
+    })
   }
+
+	render() {
+		return (
+        <>
+          <section className="result-module">
+            <div class="result" id="result">{this.textContent}</div>
+          </section>
+
+          <div className="type">
+            <div class="btn" id="start-btn">
+              {this.loading ? '正在识别' :'开始识别'}
+            </div>
+          </div>
+          </>
+		);
+	}
 }
-
-// declare global {
-//   interface HTMLElementTagNameMap {
-//     "my-app": MyApp
-//   }
-// }
-
-// import { Router } from "@vaadin/router";
-// import "./index.less";
-
-// import "./app-login";
-// import "./app-main";
-
-// const outlet = document.querySelector("#root");
-// export const router = new Router(outlet);
-
-// router.setRoutes([
-// 	{
-// 		path: "/",
-// 		component: "app-login",
-// 		action: async () => {
-// 			await import("./app-login");
-// 		},
-// 	},
-// 	{
-// 		path: "/main",
-// 		component: "app-main", // custom element name
-// 		action: async () => {
-// 			await import("./app-main");
-// 		},
-// 	},
-// 	// {
-// 	// 	path: "(.*)",
-// 	// 	component: "app-not-found",
-// 	// },
-// ]);
